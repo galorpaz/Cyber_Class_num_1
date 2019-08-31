510da52ba71807585671052e3f9d7ead353ef2e4
Administrator <Administrator@192.168.1.8>
2019-08-31 12:14:47 +0300

    first commit

---
+++ src/TurtuleExercise.java
@@ -0,0 +1,16 @@
+
+import unit4.turtleLib.Turtle;
+
+public class TurtuleExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	public static void printrectangle(int width, int height) {
+		Turtle t1 = new Turtle();
+		
+	}
+
+}
